a37573924e63e3f829cf6673a8c49c3688af08e5
Nabil <nabilkajeiou2000@gmail.com>
2022-12-27 12:13:38 +0100

    menu added, add animal function, kill animal function created

--- Ferme.py
+++ Ferme.py
@@ -1,25 +1,66 @@
 class Animal:
-    def __init__(self, nom, age):
+    def __init__(self, nom, age=0):
         self.nom = nom
         self.age = age
+
     def cri(self, cri):
         return print(cri)
 
 class Chat(Animal):
     def cri(self):
         print(type(self).__name__, self.nom, "a milaulé : Miaou!")
+    def __del__(self):
+        print(type(self).__name__, self.nom,"a été assassiné!" )
 
 class Chien(Animal):
     def cri(self):
         print(type(self).__name__, self.nom,"a aboyé: Ouaf Ouaf!" )
+    def __del__(self):
+        print(type(self).__name__, self.nom,"a été assassiné!" )
     
 class Ferme:
     def __init__(self, animaux =[]):
         self.animaux = animaux
-    def ajouter_animal(self, animal):
-        self.animaux.append(animal)
+        print("La ferme a été créé !" )
+        
+    def __del__(self):
+        print("La ferme a été détruite !" )
+    
+    def contient_animaux(self):
+        if len(self.animaux)>0:
+            return True
+        return False
+        
+    def get_noms(self):
+        if self.contient_animaux():
+            for animal in self.animaux:
+                print(animal.nom)
+        else:
+            print("Aucun animal")
+            
     def get_nb_animaux(self):
         return len(self.animaux)
+    
+    def ajouter_animal(self, animal):
+        self.animaux.append(animal)
+        print(type(animal).__name__, animal.nom,"est né!" )
+        
+    def crier(self):
+        if self.contient_animaux():
+            for animal in self.animaux:
+                animal.cri()
+        else:
+            print("Aucun animal n'a crié")
+            
+    def tuer(self,nom):
+        killed = False
+        for i in range(len(self.animaux)):
+            if self.animaux[i].nom == nom:
+                del self.animaux[i]
+                return True
+        return killed
+    
     def __str__(self):
         return "La ferme contient " + str(self.get_nb_animaux()) + " animaux."
+    
 
--- utils.py
+++ utils.py
@@ -1,12 +1,83 @@
+import os
+import csv
 from Ferme import Ferme
 from Ferme import Chien
 from Ferme import Chat
 
-chien1 = Chien("Pitbull",5)
-chien1.cri()
-chat1 = Chat("Milou",4)
-chat1.cri()
-maferme = Ferme([chien1,chat1])
-maferme.ajouter_animal(Chien("Berger Allemand", 4))
-maferme.get_nb_animaux()
-print(maferme)
+clear = lambda: os.system('cls')
+   
+exitedMenu = False
+maferme = Ferme()
+ 
+# Script du Menu
+while(exitedMenu == False):
+    print("\n[1] pour Ajouter un animal,[2] Lancer le cri des animaux, [3] Tuer un animal, [4] Voir le nombre d'animaux, [5] pour Quitter")
+    print("________________")
+
+    selectedOption = str(input("Choisissez une option : "))
+    print(selectedOption)
+    
+    # Choix listage des personnes
+    if(selectedOption== "1"):
+        clear()
+        print("| Ajout d'un nouvel animal |")
+        print("[1] pour Chat ou [2] pour Chien")
+        option = 0
+        while option != "1" and option != "2":
+            option = str(input("Choisissez une option : "))
+            
+        nom = str(input("Entrez un nom : "))
+        age = str(input("Entrez un âge : "))
+        if option == "1" :
+            nouveau = Chat(nom,age)
+        elif option == "2":
+            nouveau = Chien(nom,age)
+        clear()
+        maferme.ajouter_animal(nouveau)
+                
+
+    # Choix ajout d'une personne
+    elif(selectedOption == "2"):
+        clear()
+        print("| Cris des animaux |")
+        maferme.crier()
+
+                        
+    # Choix édition d'une personne
+    elif(selectedOption =="3"):
+        clear()
+        print("| Meurtre d'un animal |")
+        nom_animal = ""
+        if(maferme.contient_animaux()):
+            while(nom_animal == ""):
+                maferme.get_noms()
+                nom_animal = str(input("Entrez le nom de l'animal à tuer : "))
+                if nom_animal != "":
+                    clear()
+                    killed = maferme.tuer(nom_animal)
+                    if killed == False:
+                        print("Aucun animal trouvé")
+                else:
+                    print("Renseignez un nom")
+        else:
+            print("Aucun animal à tuer")
+        
+
+    # Choix suppression d'une personne
+    elif(selectedOption == "4"):
+        clear()
+        print("| Voir le nombre d'animaux |")    
+        print(maferme)    
+            
+    # Choix sortie programme
+    elif(selectedOption == "5"):
+        clear()
+        break;
+
+    # Choix incorrecte
+    else:
+        clear()
+        print("Option incorrecte")
+    
+
+print("Fin du programme")
